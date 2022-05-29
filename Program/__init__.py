c85adfcf98f6c38961041b47a8134fffb1d80dd5
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-05-29 13:37:44 +0200

    program gpio set

--- Program/__init__.py
+++ Program/__init__.py
@@ -1,3 +1,13 @@
+import machine
+import esp
+from machine import Pin, ADC, Timer
+import time
+
+#GPIO
+photo_gpio = (34,35,32,33)
+setup_btn_gpio = 25
+motors_gpio = {'yaw':12, 'pitch':13} #yaw is rotate left and right, pitch is up and down
+
 
 def setup():
     print("setup")
