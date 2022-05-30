4fd727a9a8f9acfb6e23caf5e2af0e64650e001d
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-05-30 21:48:30 +0200

    changed int to float in sttings cuz im stupid, and also changed name of that to be more precised in what the varibles give

--- Program/settings.py
+++ Program/settings.py
@@ -1,4 +1,4 @@
 from env_reader import Env_reader
 env = Env_reader()
 DEBUG = False if env.get("DEBUG", 'False') in ['0', 'False'] else True
-double_click_time = [int(i) for i in env.get("double_click_time", "(1,0.5)").strip("()").split(",")]
+time_between_multi_presses, time_from_last_relase = [float(i.strip()) for i in env.get("double_click_time", "(1,0.5)").strip("()").split(",")]
