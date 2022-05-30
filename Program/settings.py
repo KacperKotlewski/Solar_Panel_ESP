6980763a3b12daf96ce258077d87245908d46b05
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-05-30 22:07:04 +0200

    i've forgot of adding space to strips chars

--- Program/settings.py
+++ Program/settings.py
@@ -1,4 +1,4 @@
 from env_reader import Env_reader
 env = Env_reader()
 DEBUG = False if env.get("DEBUG", 'False') in ['0', 'False'] else True
-time_between_multi_presses, time_from_last_relase = [float(i.strip()) for i in env.get("double_click_time", "(1,0.5)").strip("()").split(",")]
+time_between_multi_presses, time_from_last_relase = [float(i.strip()) for i in env.get("multi_click_timeing", "(1,0.5)").strip(" ()").split(",")]
