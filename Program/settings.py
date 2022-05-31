1352053f2e3d76fc6000e3796a023e67b82b50fe
KacperKotlewski <kotlewski.kacper@gmail.com>
2022-05-31 11:23:39 +0200

    added simple webserwer to test

--- Program/settings.py
+++ Program/settings.py
@@ -2,3 +2,5 @@ from env_reader import Env_reader
 env = Env_reader()
 DEBUG = False if env.get("DEBUG", 'False') in ['0', 'False'] else True
 time_between_multi_presses, time_from_last_relase = [float(i.strip()) for i in env.get("multi_click_timeing", "(1,0.5)").strip(" ()").split(",")]
+ssid = env.get("ssid")
+passwd = env.get("passwd")
---
+++ Program/web_server.py
@@ -0,0 +1,36 @@
+from .settings import ssid, passwd
+import network
+import uasyncio
+from src.microdot_asyncio import Microdot
+
+
+def scan():
+    wlan = network.WLAN(network.STA_IF) # create station interface
+    wlan.active(True)                   # activate the interface
+    return wlan.scan()                  # scan for access points
+
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, passwd)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+    return wlan
+
+app = Microdot()
+
+def start_server():
+    print("start server")
+    connect()
+    try:
+        app.run(port=80)
+    except Exception as e:
+        app.shutdown()
+        raise Exception(e)
+    
+@app.route('/')
+def index(request):
+    return "Hello World"
